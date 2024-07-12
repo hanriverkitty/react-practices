b486df2dee010993ff063935a5f1704e8d0ef68d
hanriverkitty <danieltls@naver.com>
2024-07-12 16:28:28 +0900

    styling #finish

--- 03.Component/ex02/src/04/Banner01.js
+++ 03.Component/ex02/src/04/Banner01.js
@@ -1,11 +1,11 @@
-import React from 'react';
-
+import React from "react";
+import { Header } from "./assets/css/Banner01.css";
 function Banner01(props) {
-    return (
-        <div>
-           <h1>Hello React</h1> 
-        </div>
-    );
+  return (
+    <div>
+      <h1 className={Header}>Hello React</h1>
+    </div>
+  );
 }
 
 export default Banner01;
--- 03.Component/ex02/src/04/Banner02.js
+++ 03.Component/ex02/src/04/Banner02.js
@@ -1,11 +1,11 @@
-import React from 'react';
-
+import React from "react";
+import { Header } from "./assets/css/Banner02.css";
 function Banner02(props) {
-    return (
-        <div>
-            <h1>Hello React</h1>            
-        </div>
-    );
+  return (
+    <div>
+      <h1 className={Header}>Hello React</h1>
+    </div>
+  );
 }
 
 export default Banner02;
