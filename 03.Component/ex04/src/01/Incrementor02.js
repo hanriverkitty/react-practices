83150f8092cbf6ef8fbce1cbd4025f9485a422f2
hanriverkitty <danieltls@naver.com>
2024-07-15 20:54:53 +0900

    state

---
+++ 03.Component/ex04/src/01/Incrementor02.js
@@ -0,0 +1,21 @@
+import React, { useState } from "react";
+import Incrementor01 from "./Incrementor01";
+
+function Incrementor02({ begin, step }) {
+  const [val, setVal] = useState(begin);
+  // const [va2, setVal2] = useState(20);
+  return (
+    <div>
+      <button
+        onClick={() => {
+          setVal(val + step);
+        }}
+      >
+        <strong>{"+"}</strong>
+      </button>{" "}
+      <span>{val}</span>
+    </div>
+  );
+}
+
+export default Incrementor02;
