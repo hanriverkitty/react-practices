63b5287466573f9e41b1bcdb3567e03dab98e76e
hanriverkitty <danieltls@naver.com>
2024-07-15 13:04:42 +0900

    event #finish

--- 03.Component/ex03/src/05/App.js
+++ 03.Component/ex03/src/05/App.js
@@ -1,13 +1,30 @@
-import React from 'react';
-import './assets/scss/App.scss';
-
+import React, { useRef } from "react";
+import "./assets/scss/App.scss";
 export default function App() {
-    return (
-        <div className={'App'}>
-            <div>
-                <ul>
-                </ul>
-            </div>
-        </div>
-    );
+  const outerRef = useRef(null);
+  const innerRef = useRef(null);
+  return (
+    <div
+      ref={outerRef}
+      className={"App"}
+      onScroll={(e) => {
+        console.log(
+          outerRef.current.scrollTop,
+          outerRef.current.clientHeight,
+          innerRef.current.clientHeight
+        );
+      }}
+    >
+      <div ref={innerRef}>
+        <ul>
+          {/* {Array.from({ length: 100 }).map((_, i) => (
+            <li>아이템 ${i + 1}입니다</li>
+          ))} */}
+          {Array.from({ length: 100 }, (_, i) => i + 1).map((e) => (
+            <li key={e}>{`아이템 ${e}입니다`}</li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
 }
