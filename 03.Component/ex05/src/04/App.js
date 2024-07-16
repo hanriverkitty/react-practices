e23701a97d4853a3e48e82d05c089e9ebb471fbd
hanriverkitty <danieltls@naver.com>
2024-07-16 15:04:32 +0900

    Clock2

--- 03.Component/ex05/src/04/App.js
+++ 03.Component/ex05/src/04/App.js
@@ -1,15 +1,41 @@
-import React, {useState, useEffect} from 'react';
-import './assets/scss/App.scss'
-import Clock from './Clock';
+import React, { useState, useEffect } from "react";
+import "./assets/scss/App.scss";
+import Clock from "./Clock";
 
 export default function App() {
-    return(
-        <>
-            <Clock
-                title={`ex05: Clock Component II: 0`}
-                hours={12}
-                minutes={0}
-                seconds={0} />
-        </>
-    );    
+  const getCurrentTime = function () {
+    const now = new Date();
+    return {
+      hours: now.getHours(),
+      minutes: now.getMinutes(),
+      seconds: now.getSeconds(),
+    };
+  };
+
+  const [currentTime, setCurrentTime] = useState(getCurrentTime());
+  const [tick, setTick] = useState(0);
+
+  useEffect(
+    () => {
+      const intervalid = setInterval(() => {
+        setCurrentTime(getCurrentTime());
+        //   setTick(tick + 1);
+        setTick((x) => x + 1);
+      }, 1000);
+
+      return () => {
+        clearInterval(intervalid);
+      };
+    } /*[tick]*/
+  );
+  return (
+    <>
+      <Clock
+        title={`ex05: Clock Component II: ${tick}`}
+        hours={currentTime.hours}
+        minutes={currentTime.minutes}
+        seconds={currentTime.seconds}
+      />
+    </>
+  );
 }
