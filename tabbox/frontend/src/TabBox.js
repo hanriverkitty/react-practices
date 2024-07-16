8d103a93d4d8c6d042375cb0fb685dbe8c67f7dc
hanriverkitty <danieltls@naver.com>
2024-07-16 09:53:34 +0900

    tabbox select teacher .ver

--- tabbox/frontend/src/TabBox.js
+++ tabbox/frontend/src/TabBox.js
@@ -5,17 +5,28 @@ import { Tab_Box } from "./assets/scss/TabBox.scss";
 import tabs from "./assets/json/data";
 
 function TabBox() {
-  const [idx, setIdx] = useState(tabs[0].no);
+  // const [idx, setIdx] = useState(tabs[0].no);
+  const [idx, setIdx] = useState(0);
+
+  const selectTab = function (no) {
+    // const index = tabs.map((e) => e.no).indexOf(no);
+    // setIdx(index);
+    setIdx(tabs.findIndex((e) => e.no === no));
+  };
 
   return (
     <div className={Tab_Box}>
       <Tabs
-        selectTab={(no) => {
-          setIdx(no);
-        }}
-        tabs={tabs.map((e) => {
+        // selectTab={(no) => {
+        //   setIdx(no);
+        // }}
+        selectTab={selectTab}
+        tabs={tabs.map((e, i) => {
           const { contents, ...rest } = e;
-          rest.no === idx ? (rest.active = true) : "";
+          // rest.no === idx ? (rest.active = true) : "";
+          if (i === idx) {
+            rest.active = true;
+          }
           return rest;
         })}
       />
