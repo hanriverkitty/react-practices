050336b740e7c576a813b82b7aed5151d7b010b2
hanriverkitty <danieltls@naver.com>
2024-07-18 17:35:08 +0900

    Routing

--- 04.Routing/src/02/App.js
+++ 04.Routing/src/02/App.js
@@ -1,54 +1,64 @@
-import React, {useState} from 'react';
+import React, { useState, useEffect } from "react";
 import Main from "./component/Main";
 import Gallery from "./component/Gallery";
 import Guestbook from "./component/Guestbook";
 import Error404 from "./component/Error404";
 
 export default function App() {
-    const [route, setRoute] = useState({});
+  const [route, setRoute] = useState({
+    page: window.location.hash.substring(window.location.href.lastIndexOf("/")),
+  });
 
-    const handleLinkClick = (e) => {
-    }
+  const hadnlerPopState = (e) => {
+    setRoute(e.state ? e.state : { page: "/" });
+  };
 
-    const Router = function () {
-        let component = null;
+  const handleLinkClick = (e) => {
+    e.preventDefault();
+    const url = e.target.href.substring(e.target.href.lastIndexOf("/"));
+    console.log(url);
+    window.history.pushState({ page: url }, e.target.text, url);
+    setRoute({ page: url });
+  };
 
-        switch (route.page) {
-            case '/':
-                component = <Main/>;
-                break;
-            case '/gallery':
-                component = <Gallery/>;
-                break;
-            case '/guestbook':
-                component = <Guestbook/>;
-                break;
-        }
-
-        return component;
+  useEffect(() => {
+    window.addEventListener("popstate", hadnlerPopState);
+    return () => {
+      window.removeEventListener("popstate", hadnlerPopState);
     };
+  }, []);
 
-    return (
-        <div>
-            <ul>
-                <li><a href={'/'} onClick={handleLinkClick}>[Main]</a></li>
-                <li><a href={'/gallery'} onClick={handleLinkClick}>[Gallery]</a></li>
-                <li><a href={'/guestbook'} onClick={handleLinkClick}>[Guestbook]</a></li>
-            </ul>
-            {
-                (() => {
-                    switch(route.page) {
-                        case '/' :
-                            return <Main />;
-                        case '/guestbook' :
-                            return <Guestbook />;
-                        case '/gallery':
-                            return <Gallery />;
-                        default :
-                            return <Error404 />;                    
-                    }
-                })()
-            }
-        </div>
-    )
+  return (
+    <div>
+      <ul>
+        <li>
+          <a href={"/"} onClick={handleLinkClick}>
+            [Main]
+          </a>
+        </li>
+        <li>
+          <a href={"/gallery"} onClick={handleLinkClick}>
+            [Gallery]
+          </a>
+        </li>
+        <li>
+          <a href={"/guestbook"} onClick={handleLinkClick}>
+            [Guestbook]
+          </a>
+        </li>
+      </ul>
+      {(() => {
+        switch (route.page) {
+          case "/":
+            return <Main />;
+          case "/guestbook":
+            return <Guestbook />;
+          case "/gallery":
+            return <Gallery />;
+          default:
+            return <Error404 />;
+        }
+      })()}
+    </div>
+  );
 }
