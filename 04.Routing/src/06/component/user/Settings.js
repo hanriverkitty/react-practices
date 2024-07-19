e1e93fa1ea64629a3741b8f70b83b8db0737f8b0
hanriverkitty <danieltls@naver.com>
2024-07-19 13:06:35 +0900

    router #finish

--- 04.Routing/src/06/component/user/Settings.js
+++ 04.Routing/src/06/component/user/Settings.js
@@ -1,12 +1,19 @@
-import React, {useEffect} from 'react';
+import React from "react";
+import { SiteLayout } from "../../layout";
 
 export default function Settings() {
-    return (
-            <div>
-                <h2 style={{
-                    lineHeight: '200px',
-                    textAlign: 'center'
-                }}>User - Settings</h2>
-            </div>
-    );
+  return (
+    <SiteLayout>
+      <div>
+        <h2
+          style={{
+            lineHeight: "200px",
+            textAlign: "center",
+          }}
+        >
+          User - Settings
+        </h2>
+      </div>
+    </SiteLayout>
+  );
 }
