b368a5ea08c26e3dba1d04e13fd78891481854d0
hanriverkitty <danieltls@naver.com>
2024-07-24 18:04:12 +0900

    backend finish

---
+++ kanbanboard/backend/src/main/java/com/poscodx/kanbanboard/controller/ApiController.java
@@ -0,0 +1,60 @@
+package com.poscodx.kanbanboard.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.poscodx.kanbanboard.dto.JsonResult;
+import com.poscodx.kanbanboard.repository.KanbanboardRepository;
+import com.poscodx.kanbanboard.vo.TaskVo;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RestController
+public class ApiController {
+	
+	@Autowired
+	private KanbanboardRepository kanbanboardRepository;
+	
+	@DeleteMapping("/api/delete")
+	public ResponseEntity<JsonResult> delete(@RequestParam(value="no", required=true, defaultValue="") Long no){
+		log.info("Request[DELETE /api]: "+ no);
+		kanbanboardRepository.delete(no);
+		return ResponseEntity
+					.status(HttpStatus.OK)
+					.body(JsonResult.success(no));
+	}
+	
+	@GetMapping("/api/card")
+	public ResponseEntity<JsonResult> readCard(){
+		log.info("Request[GET /api]: ");
+		return ResponseEntity
+					.status(HttpStatus.OK)
+					.body(JsonResult.success(kanbanboardRepository.findCardAll()));
+	}
+	@GetMapping("/api/task")
+	public ResponseEntity<JsonResult> readTask(Long no){
+		log.info("Request[GET /api]: ");
+		return ResponseEntity
+					.status(HttpStatus.OK)
+					.body(JsonResult.success(kanbanboardRepository.findTaskAll(no)));
+	}
+	
+	@PostMapping("/api/insert")
+	public ResponseEntity<JsonResult> create(@RequestBody TaskVo vo){
+		log.info("Request[POST /api]: "+ vo);
+		kanbanboardRepository.insert(vo);
+		return ResponseEntity
+					.status(HttpStatus.OK)
+					.body(JsonResult.success(vo));
+	}
+	
+	
+}
---
+++ kanbanboard/backend/src/main/java/com/poscodx/kanbanboard/repository/KanbanboardRepository.java
@@ -0,0 +1,34 @@
+package com.poscodx.kanbanboard.repository;
+
+import java.util.List;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.poscodx.kanbanboard.vo.CardVo;
+import com.poscodx.kanbanboard.vo.TaskVo;
+
+@Repository
+public class KanbanboardRepository {
+
+	@Autowired
+	private SqlSession sqlSession;
+	
+	public List<CardVo> findCardAll(){
+		return sqlSession.selectList("kanbanboard.findCardAll");
+	}
+	
+	public List<TaskVo> findTaskAll(Long no){
+		return sqlSession.selectList("kanbanboard.findTaskAll",no);
+	}
+
+	public int insert(TaskVo vo) {
+		return sqlSession.insert("kanbanboard.insert",vo);
+	}
+
+	public int delete(Long no) {
+		return sqlSession.delete("kanbanboard.delete",no);
+		
+	}
+}
